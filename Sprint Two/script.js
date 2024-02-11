73034e4f2ede42a09c59d666fe163c6e1bfe772d
Vinnicius Castro <vinnicius.castro97@gmail.com>
2024-02-10 23:01:37 -0700

    vamo ver

--- Sprint Two/script.js
+++ Sprint Two/script.js
@@ -1,7 +1,21 @@
-function convertCurrency() {
-    var amount = document.getElementById('amount').value;
-    var rate = document.getElementById('currency').value;
-    var convertedAmount = amount * rate;
-
-    document.getElementById('result').innerHTML = 'Converted Amount: ' + convertedAmount.toFixed(2);
+function httpGetAsync(url, callback) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            callback(xmlHttp.responseText);
+        }
+    };
+    xmlHttp.open("GET", url, true); // true for asynchronous
+    xmlHttp.send(null);
 }
+
+const url = "https://exchange-rates.abstractapi.com/v1/live/?api_key=6e2c4cc0ca704f15a5e67a41b3c09d97&base=USD&target=EUR";
+
+// Define a callback function to handle the response
+function handleApiResponse(response) {
+    // Process the response as needed
+    console.log(response);
+}
+
+// Make the API request
+httpGetAsync(url, handleApiResponse);
